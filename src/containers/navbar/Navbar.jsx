ddd2d03d5d40fc299677f4a96ed295146dc55038
BAstien <aletrut.bastien@gmail.com>
2023-03-31 16:58:06 +0200

    update

--- src/containers/navbar/Navbar.jsx
+++ src/containers/navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Logo from "../../assets/x.svg";
 import "./navbar.css";
 
 const Navbar = () => {
@@ -11,21 +10,29 @@ const Navbar = () => {
 
   return (
     <div class="menu">
-      <img className="icon" src={Logo} alt="" onClick={handleLogoClick} />
-      <div className={`menu-nav ${isOpened ? "menu-opened" : ""}`}>
-        <img className="icon" src={Logo} alt="" onClick={handleLogoClick} />
+      {/* <img className="icon" src={Logo} alt="" onClick={handleLogoClick} /> */}
+      <div className={`menu-icon1 ${isOpened ? "menu-icon1-1" : ""}`}>
+
+        <div className={`menu-icon2 ${isOpened ? "menu-icon2-1" : ""}`} onClick={handleLogoClick}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+    </div>
+    <div className={`menu-nav ${isOpened ? "menu-opened" : ""}`}>
+        
         <div className="menu-ligne">MICKAEL OLANIER</div>
         <div className="menu-items">
-          <a className="menu-item" href="#Accueil">
+          <a className="menu-item" href="#Accueil" onClick={handleLogoClick}>
             Accueil
           </a>
-          <a className="menu-item" href="#presentation">
+          <a className="menu-item" href="#presentation" onClick={handleLogoClick}>
             Presentation
           </a>
-          <a className="menu-item" href="#prestation">
+          <a className="menu-item" href="#prestation" onClick={handleLogoClick}>
             Prestation
           </a>
-          <a className="menu-item" href="#contact">
+          <a className="menu-item" href="#contact" onClick={handleLogoClick}>
             Me Contacter
           </a>
         </div>
