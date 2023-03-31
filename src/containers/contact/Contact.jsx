7ab582442b5c4007e07358fcc81cf53187bc3487
bastien <aletrut.bastien@gmail.com>
2023-03-31 11:36:40 +0100

    update

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import './contact.css';
+import React from "react";
+import "./contact.css";
+
+import instalogo from "../../assets/instalogo.png";
 const Contact = () => (
-      <div className="con-all">
-      </div>
+  <div className="con-all">
+    <div id="lien">
+      <a href="https://www.instagram.com/_fitmack/?hl=fr">
+        <div id="lien-reseau">
+          <div id="titre2">
+            <img id="instalogo" src={instalogo} alt="" />
+            <h1>fitmack</h1>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
 );
 
 export default Contact;
