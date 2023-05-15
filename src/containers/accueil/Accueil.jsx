6170847c339375246126c4cd24a0053d51ac5d1e
BAstien <aletrut.bastien@gmail.com>
2023-05-15 13:54:46 +0200

    Update Accueil.jsx

--- src/containers/accueil/Accueil.jsx
+++ src/containers/accueil/Accueil.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import './accueil.css';
 import img from "../../assets/Fitmack2.png";
-const Accueil = () => (
+
+
+
+const Accueil = () => {
+
+  function scrollToContact() {
+    const Contact = document.querySelector('#con-all');
+    Contact.scrollIntoView({
+      smooth: true
+    });
+  }
+  return (
       <div id='Accueil' className="acc-all">
           
           <div className="acc-title">MICKAEL <span class="acc-sun">O</span>LANIER</div>
           <div className="acc-cnt">
             <div className="acc-txt">“Ce qui sépare l’information de la transformation ? Le passage à l’action”</div>
-            <div className="acc-btn">Passer à l'action !</div>
+            <a href="#con-all" onClick={scrollToContact}>
+              <div className="acc-btn">Passer à l'action !</div>
+            </a>
+            
           </div>
           
           <div className="acc-image">
@@ -15,7 +29,7 @@ const Accueil = () => (
                     <img className="acc-bando" src={img} alt=""></img>
                 </div>
           </div>
-      </div>
-);
+      </div>);
+}
 
 export default Accueil;
