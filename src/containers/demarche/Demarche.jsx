030050832da49a32d806b0e7c37556e3a0c014dd
BAstien <aletrut.bastien@gmail.com>
2023-05-15 15:23:44 +0200

    update

--- src/containers/demarche/Demarche.jsx
+++ src/containers/demarche/Demarche.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import "./demarche.css";
-import "https://kit.fontawesome.com/c6ee84be12.js";
 import { Link } from "react-router-dom";
-
+import logo from "../../assets/chevron.png"
 function Demarche() {
     return(
         <Link to="/demarche">
+            
             <div className="dem-all">
                 <div className="dem-title">
                     DEMARCHE 
                 </div>
-                <span className="dem-arrow">
-                    <i className="dem-icon1 fas fa-chevron-right"></i>
-                    <i className="dem-icon2 fas fa-chevron-right"></i>
-                    <i className="dem-icon3 fas fa-chevron-right"></i>
-                </span>
+                <div className="dem-arrow">
+                    <img className="dem-icon" src={logo} alt="" />
+                    <img className="dem-icon" src={logo} alt="" />
+                    <img className="dem-icon" src={logo} alt="" />
+                </div>
             </div>
+            
         </Link>
     );
 }
